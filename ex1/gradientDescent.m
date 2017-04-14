a682a0d5b6d9e111c3ec2b1b11f744098fff4c31
Daniil K <dkubatko@ucsd.edu>
2017-04-13 17:49:51 -0700

    Frad

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,7 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    disp 
+    
+    theta = theta - alpha * 1 / m * sum(X' * (X*theta - y))
 
 
 
